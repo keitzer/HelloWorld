b8aeb4d168fbfb8fb7850fead9d4126cef89cfbe
Alex Ogorek <ogorek@usc.edu>
2014-09-01 09:23:33 -0700

    My first HelloWorld using git

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.cpp
+//  C++Projects
+//
+//  Created by Alex Ogorek on 9/1/14.
+//  Copyright (c) 2014 Alex Ogorek. All rights reserved.
+//
+
+#include "HelloWorld.h"
+int main()
+{
+	std::cout << "HelloWorld!" << std::endl;
+}
---
+++ HelloWorld.h
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.h
+//  C++Projects
+//
+//  Created by Alex Ogorek on 9/1/14.
+//  Copyright (c) 2014 Alex Ogorek. All rights reserved.
+//
+
+#ifndef __C__Projects__HelloWorld__
+#define __C__Projects__HelloWorld__
+
+#include <iostream>
+
+#endif /* defined(__C__Projects__HelloWorld__) */
